534276d40ae73c90dc6f3129a95e8e947bd7fc31
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-30 17:55:38 -0400

    basic project setup

---
+++ Collections/Collections/FISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FISAppDelegate.h
+//  Collections
+//
+//  Created by Chris Gonzales on 5/30/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Collections/Pods/Pods-CollectionsTests-Swizzlean-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CollectionsTests_Swizzlean : NSObject
+@end
+@implementation PodsDummy_Pods_CollectionsTests_Swizzlean
+@end
