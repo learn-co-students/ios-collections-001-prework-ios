2117f52869d62cc58e02ff870ef5a053e74f63f6
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-30 18:02:09 -0400

    add FISCollections

---
+++ Collections/Collections/FISCollections.h
@@ -0,0 +1,15 @@
+//
+//  FISCollections.h
+//  Collections
+//
+//  Created by Chris Gonzales on 5/30/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISCollections : NSObject
+
+//declare methods here
+
+@end
---
+++ Collections/Collections/FISCollections.m
@@ -0,0 +1,15 @@
+//
+//  FISCollections.m
+//  Collections
+//
+//  Created by Chris Gonzales on 5/30/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import "FISCollections.h"
+
+@implementation FISCollections
+
+//define methods here
+
+@end
