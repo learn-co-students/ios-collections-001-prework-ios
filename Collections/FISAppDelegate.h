56774109eeb2c94ba89a809a0dcad63bb3f7ae21
wojunfeng <wjun1924@gmail.com>
2015-09-03 21:32:12 -0400

    prework searching and sorting practice

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -17,5 +17,29 @@
  * Declare your methods here.
  
  */
+//1
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)array;
+//2
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)array;
+//3
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array;
+//4
+- (NSArray *)arrayByReversingArray:(NSArray *)array;
+//5
+- (NSString *)stringInBasicLeetFromString:(NSString *)string;
+//6
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array;
+//7
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)dictionary;
+//8
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)array;
+//9
+- (NSInteger)sumOfIntegersInArray:(NSArray *)array;
+//10
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array;
+//11
+- (NSDictionary *)countsOfWordsInString:(NSString *)string;
+//12
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)array;
 
 @end
