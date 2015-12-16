21d8fcdfe711e3d5311f0e0d2043df29f7717a3f
Dnld <steinert.donald@gmail.com>
2015-12-16 15:31:04 -0500

    implements method skeletons

--- Collections/FISAppDelegate.m
+++ Collections/FISAppDelegate.m
@@ -16,4 +16,64 @@
  
  */
 
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)arrayByReversingArray:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)stringInBasicLeetFromString:(NSString *)string
+{
+    return nil;
+}
+
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)hobbits
+{
+    return nil;
+}
+
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSInteger)sumOfIntegersInArray:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array
+{
+    return nil;
+}
+
+- (NSDictionary *)countsOfWordsInString:(NSString *)string
+{
+    return nil;
+}
+
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)songs
+{
+    return nil;
+}
+
 @end
