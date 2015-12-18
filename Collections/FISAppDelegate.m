00faffd8788b11817ddfd231810fdee6dc7a4793
Shena Yoshida <sheyoshida@gmail.com>
2015-12-18 15:26:01 -0500

    set up methods

--- Collections/FISAppDelegate.m
+++ Collections/FISAppDelegate.m
@@ -10,10 +10,64 @@
     return YES;
 }
 
-/**
- 
- * Define your methods here.
- 
- */
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSArray *)arrayByReversingArray:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSString *)stringInBasicLeetFromString:(NSString *)string {
+    
+    return nil;
+}
+
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)dictionary {
+    
+    return nil;
+}
+
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSInteger *)sumOfIntegersInArray:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)array {
+    
+    return nil;
+}
+
+- (NSDictionary *)countsOfWordsInString:(NSString *)string {
+    
+    return nil;
+}
+
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)array {
+    
+    return nil;
+}
 
 @end
