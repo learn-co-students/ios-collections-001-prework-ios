fc90375b7449672811244b636860435266afd869
ganderson09 <anderson.gabriel09@gmail.com>
2016-03-18 15:40:43 -0400

    Collections Practice Complete - Tests Passed

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -6,10 +6,28 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)ascArray;
+
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)descArray;
+
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)objectArray;
+
+- (NSArray *)arrayByReversingArray:(NSArray *)reverseArray;
+
+- (NSString *)stringInBasicLeetFromString:(NSString *)leetString;
+
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)splitArray;
+
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)hobbitsDictionary;
+
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)aInArray;
+
+- (NSInteger)sumOfIntegersInArray:(NSArray *)integersArray;
+
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)pluralsArray;
+
+- (NSDictionary *)countsOfWordsInString:(NSString *)countsString;
+
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)songsArray;
 
 @end
