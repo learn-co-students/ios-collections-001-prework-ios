e9257c1d1d815120ad99e48e2e46dd2fd1795a8a
yayitserica <ericamillado@gmail.com>
2016-04-09 22:00:05 -0400

    done with collections prework

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -6,10 +6,22 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+//__block FISAppDelegate *appDelegate;
+
+
+-(NSArray *)arrayBySortingArrayAscending:(NSArray *) chaoticPrimes;
+-(NSArray *)arrayBySortingArrayDescending:(NSArray *)chaoticPrimes;
+-(NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)array;
+-(NSArray *)arrayByReversingArray:(NSArray *)array;
+-(NSString *)stringInBasicLeetFromString:(NSString *)string;
+-(NSInteger )sumOfIntegersInArray:(NSArray *)chaoticPrimes;
+-(NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)numbersToBisect;
+-(NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)fellowshipOfTheRing;
+-(NSArray *)stringsBeginningWithAInArray:(NSArray *)foodsBeginningWithA;
+-(NSArray *)arrayByPluralizingStringsInArray:(NSArray *)singularArray;
+-(NSDictionary *)countsOfWordsInString:(NSString *)string;
+-(NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)songsByArtist;
+
+
 
 @end
