8511de805595d1840fcffcfab9a76bc0b42320df
Cordavi <michaelamundsen@gmail.com>
2016-05-02 19:37:01 -0400

    Declared methods in header and set them to return nil till defined

--- Collections/FISAppDelegate.m
+++ Collections/FISAppDelegate.m
@@ -10,10 +10,53 @@
     return YES;
 }
 
-/**
- 
- * Define your methods here.
- 
- */
+-(NSArray *)arrayBySortingArrayAscending:(NSArray *)arrayToSort {
+    return nil;
+}
+
+-(NSArray *)arrayBySortingArrayDescending:(NSArray *)arrayToSort {
+    
+    return nil;
+}
+
+-(NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)arrayToSwap {
+    return nil;
+}
+
+-(NSArray *)arrayByReversingArray:(NSArray *)arrayToReverse {
+    return nil;
+}
+
+-(NSString *)stringInBasicLeetFromString:(NSString *)leetString {
+    return nil;
+}
+
+-(NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)negativeAndPositiveArray {
+    return nil;
+}
+
+-(NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)hobbitDictionary {
+    return nil;
+}
+
+-(NSArray *)stringsBeginningWithAInArray:(NSArray *)arrayOfStrings {
+    return nil;
+}
+
+-(NSInteger)sumOfIntegersInArray:(NSArray *)integerArray {
+    return 0;
+}
+
+-(NSArray *)arrayByPluralizingStringsInArray:(NSArray *)stringArray {
+    return nil;
+}
+
+-(NSDictionary *)countsOfWordsInString:(NSString *)string {
+    return nil;
+}
+
+-(NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)songsArray {
+    return nil;
+}
 
 @end
