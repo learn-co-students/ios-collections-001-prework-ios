64cad5146472442b7a11144da1d187f7494f28de
Uskinn <s.nevzor@yahoo.com>
2016-05-05 17:45:48 -0400

    Done

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -12,4 +12,29 @@
  
  */
 
+-(NSArray *)arrayBySortingArrayAscending: (NSArray *)ascendingPrimes;
+
+-(NSArray *)arrayBySortingArrayDescending: (NSArray *)descendingPrimes;
+
+-(NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray: (NSArray *)givenStrings;
+
+-(NSArray *)arrayByReversingArray: (NSArray *)reverseArray;
+
+-(NSString *)stringInBasicLeetFromString: (NSString *)subStrings;
+
+-(NSArray *)splitArrayIntoNegativesAndPositives: (NSArray *)negPosArray;
+
+-(NSArray *)namesOfHobbitsInDictionary: (NSDictionary *)middleEarth;
+
+-(NSArray *)stringsBeginningWithAInArray: (NSArray *)aStrings;
+
+-(NSInteger)sumOfIntegersInArray: (NSArray *)integersArray;
+
+-(NSArray *)arrayByPluralizingStringsInArray: (NSArray *)stringsArray;
+
+-(NSDictionary *)countsOfWordsInString: (NSString *)wordsString;
+
+-(NSDictionary *)songsGroupedByArtistFromArray: (NSArray *)artistArray;
+
+
 @end
